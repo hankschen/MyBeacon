69e0fffc9685cfbceb64020e1c214a11633221d0
Hanks <cmnofadsl@gmail.com>
2016-12-28 20:35:15 +0800

    Android-Bluetooth Low Energy(BLE) 基本操作( 一 )

--- app/src/main/java/com/example/hanks/mybeacon/MainActivity.java
+++ app/src/main/java/com/example/hanks/mybeacon/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hanks.mybeacon;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,9 +39,44 @@ public class MainActivity extends AppCompatActivity {
         //檢查手機是否開啟藍芽功能
         if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()){
             Toast.makeText(this,"請開啟藍芽功能",Toast.LENGTH_SHORT).show();
+            //若沒有開啟則顯示允許用戶打開藍牙的系統活動
             Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBluetooth, REQUEST_ENABLE_BT);
+        }else{
+            scanLeDevice(true);
         }
-
     }
+
+    // App在onPause生命週期時，停止掃瞄
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+    }
+
+    // 掃瞄藍牙裝置自訂方法
+    private void scanLeDevice(final boolean enable){
+        if (enable){
+            // postDelayed(Runnable r, long delayMillis)
+            // Causes the Runnable r to be added to the message queue, to be run after the specified amount of time elapses.
+            // 在經過指定的delayMillis時間後，才將 "r" 加到消息佇列中
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mBluetoothAdapter.stopLeScan(mLeScanCallBack);
+                }
+            }, SCAN_PERIOD);
+            mBluetoothAdapter.startLeScan(mLeScanCallBack);
+        }else {
+            mBluetoothAdapter.stopLeScan(mLeScanCallBack);
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallBack = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            //搜尋回饋
+            Log.d("TAG", "BLE device: " + device.getName());
+        }
+    };
 }
